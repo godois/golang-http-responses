523746fd598ad4cd873054c26f965e7f46e2b501
Marcio Godoi <souzagodoi@gmail.com>
2017-03-25 00:08:48 -0300

    Updating name references

---
+++ xml/main/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/xml"
+	"net/http"
+)
+
+type Profile struct {
+	Name    string
+	Hobbies []string `xml:"Hobbies>Hobby"`
+}
+
+func main() {
+	http.HandleFunc("/", responseBuffer)
+	http.ListenAndServe(":3000", nil)
+}
+
+func responseBuffer(w http.ResponseWriter, r *http.Request) {
+
+	profile := Profile{"Godois", []string{"cooking", "programming"}}
+
+	x, err := xml.MarshalIndent(profile, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write(x)
+}
