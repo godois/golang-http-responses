57cd019c8fc7996295ee394d69417471ffa5a093
Marcio Godoi <souzagodoi@gmail.com>
2017-03-25 00:05:12 -0300

    Creating xml renderization example

--- json/main/main.go
+++ json/main/main.go
@@ -6,24 +6,24 @@ import (
 )
 
 type Person struct {
-Name    string
-Hobbies []string
+	Name    string
+	Hobbies []string
 }
 
 func main() {
-http.HandleFunc("/", foo)
-http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":3000", nil)
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
-profile := Person{"Alex", []string{"snowboarding", "programming"}}
+	profile := Person{"Alex", []string{"snowboarding", "programming"}}
 
-js, err := json.Marshal(profile)
-if err != nil {
-http.Error(w, err.Error(), http.StatusInternalServerError)
-return
-}
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-w.Header().Set("Content-Type", "application/json")
-w.Write(js)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
